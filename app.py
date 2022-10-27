33b113193938cfbe5869a6f3f0f47d19c32be516
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2022-10-27 16:09:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -359,6 +359,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            body {background-color: #fff;}
            
             </style>
             """
