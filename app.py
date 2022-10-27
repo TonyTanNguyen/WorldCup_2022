af80f0b7885f1b9b32c3b08889a4023718308b25
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2022-10-27 13:31:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -359,8 +359,8 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            .viewerBadge_container__1QSob {visibility: hidden;}
-            .viewerBadge_link__1S137 {visibility: hidden;}
+            viewerBadge_container__1QSob {visibility: hidden;display: none;}
+            viewerBadge_link__1S137 {visibility: hidden;display: none;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
