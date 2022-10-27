7f195afed09572f36f9d7716ae329f535c390beb
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2022-10-27 17:07:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def main():
     # quater_final pairs
     round_of_16_matches = []
 #     st.markdown("\n\n\n#### =================== ROUND OF 16 ===================")
-    st.markdown("<h2 style='text-align: center';>=================== ROUND OF 16 ==================='</h2>",unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center';>ROUND OF 16'</h2>",unsafe_allow_html=True)
     for i in range(0,len(round_of_16_pairs),2):
         winner_group_1 = round_of_16_pairs[i][0][0]
         runners_up_group_2 = round_of_16_pairs[i+1][0][1]
@@ -163,7 +163,7 @@ def main():
     quarter_final_df = pd.DataFrame(quater_final_list,columns=['Team 1','Team 2'])
     
 #     st.markdown('\n#### ========== QUATER-FINALS ==========')
-    st.markdown("<h2 style='text-align: center';>========== QUATER-FINALS =========='</h2>",unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center';>QUATER-FINALS'</h2>",unsafe_allow_html=True)
     st.table(quarter_final_df.reset_index(drop=True))
 
 
@@ -193,7 +193,7 @@ def main():
     semi_final_df = pd.DataFrame(semi_final_list,columns=['Team 1','Team 2'])
     
 #     st.markdown('\n#### ========== SEMI-FINALS ==========')
-    st.markdown("<h2 style='text-align: center';>========== SEMI-FINALS =========='</h2>",unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center';>SEMI-FINALS'</h2>",unsafe_allow_html=True)
     st.table(semi_final_df)
 
     grand_final_list = []
@@ -222,7 +222,7 @@ def main():
     grand_final_df = pd.DataFrame(grand_final_list,columns=['Team 1','Team 2'])
     
 #     st.markdown('\n#### ========== GRAND-FINALS ==========')
-    st.markdown("<h2 style='text-align: center';>========== GRAND-FINALS =========='</h2>",unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center';>GRAND-FINALS'</h2>",unsafe_allow_html=True)
     st.table(grand_final_df)
 
     champion = ''
@@ -244,7 +244,7 @@ def main():
     st.markdown('\n\n\n\n======= GRAND-FINALS RESULT IN TABLE ========')
     st.table(grand_final_df.reset_index(drop=True))
 
-    st.markdown('\n\n\n\n****======= FIFA WOURLD CUP 2022 CHAMPIONS=======****')
+#     st.markdown('\n\n\n\n****======= FIFA WOURLD CUP 2022 CHAMPIONS=======****')
     st.markdown("<h2 style='text-align: center'; color:'red'>FIFA WOURLD CUP 2022 CHAMPIONS'</h2>",unsafe_allow_html=True)
     st.markdown(f"## {champion}")
 
