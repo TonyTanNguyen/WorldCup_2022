038cc3a30be6ae94780ebaf86b1b69e5e1fb0575
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2022-11-16 16:49:54 +0700

    Add files via upload

--- functions.py
+++ functions.py
@@ -74,30 +74,30 @@ def result_score(my_df):
             score_dict[team2] += 3
     return score_dict
 
-def cal_percent_quater(col):
+def cal_percent_quater(col,time_to_simulate):
     try:
 #         return (quater_dict[col['Team']]/country_proba[col['Team']]) * col['Percent_to_group_16']
-        return (quater_dict[col['Team']]*100/1000)
+        return (quater_dict[col['Team']]*100/time_to_simulate)
     except:
         return 0
     
-def cal_percent_semi(col):
+def cal_percent_semi(col,time_to_simulate):
     try:
 #         return (semi_dict[col['Team']]/quater_dict[col['Team']]) * col['Percent_to_quater']
-        return (semi_dict[col['Team']]*100/1000)
+        return (semi_dict[col['Team']]*100/time_to_simulate)
     except:
         return 0 
     
-def cal_percent_final(col):
+def cal_percent_final(col,time_to_simulate):
     try:
 #         return (final_dict[col['Team']]/semi_dict[col['Team']]) * col['Percent_to_semi']
-        return (final_dict[col['Team']]*100/1000)
+        return (final_dict[col['Team']]*100/time_to_simulate)
     except:
         return 0  
     
-def cal_percent_cham(col):
+def cal_percent_cham(col,time_to_simulate):
     try:
-        return (champion_dict[col['Team']]*100/1000)
+        return (champion_dict[col['Team']]*100/time_to_simulate)
 #         return (champion_dict[col['Team']]/final_dict[col['Team']]) * col['Percent_to_final']
     except:
         return 0  
