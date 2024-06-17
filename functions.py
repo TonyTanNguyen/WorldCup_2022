87cd7b97a54981096833c1497028b656e15b7f68
TonyTanNguyen <100524788+TonyTanNguyen@users.noreply.github.com>
2024-06-17 16:36:39 +0700

    my commit

--- functions.py
+++ functions.py
@@ -17,11 +17,11 @@ def predict_games(model_home,model_away,stage,knock_out=False):
     if knock_out:
         home_score = model_home.predict(stage[predict_col])
         away_score = model_away.predict(stage[predict_col])
-        home_score = [random.uniform(0.0, 1.0) + i for i in home_score]
-        away_score = [random.uniform(0.0, 1.0) + i for i in away_score]
-    else:
-        home_score = [round(random.uniform(0.0, 1.0) + i,1) for i in home_score]
-        away_score = [round(random.uniform(0.0, 1.0) + i,1) for i in away_score]
+        # home_score = [random.uniform(0.0, 1.0) + i for i in home_score]
+        # away_score = [random.uniform(0.0, 1.0) + i for i in away_score]
+    # else:
+        # home_score = [round(random.uniform(0.0, 1.0) + i,1) for i in home_score]
+        # away_score = [round(random.uniform(0.0, 1.0) + i,1) for i in away_score]
         home_score = [round_number(i) for i in home_score]
         away_score = [round_number(i) for i in away_score]
 
